6638ba5857a082215dfad0c5c734491c8a6ede64
mahin651 <mahinmittal2018@gmail.com>
2020-12-15 18:15:51 +0530

    footer added

--- public/js/app.js
+++ public/js/app.js
@@ -79869,8 +79869,8 @@ function Navigation() {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/sanchit/TNP-WEBSITE/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/sanchit/TNP-WEBSITE/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/mahinmittal/TNP-WEBSITE/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/mahinmittal/TNP-WEBSITE/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
