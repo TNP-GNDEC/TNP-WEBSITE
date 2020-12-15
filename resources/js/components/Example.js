193cc17f00435f8f744606bde38c3f7f832a6523
Sanchit987 <kherasanchit1@gmail.com>
2020-12-15 11:48:47 +0530

    Base Code added

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Navigation from "./Navigation";
+
 
 function Example() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
-
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <Navigation />
         </div>
     );
 }
