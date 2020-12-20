d0a5775a6b187f4f063995ed5a4b94b56d478335
Sanchit987 <kherasanchit1@gmail.com>
2020-12-20 08:04:57 +0530

    Added

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -2,11 +2,6 @@ import { ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createMuiTheme } from '@material-ui/core/styles';
-import { Theme } from './Theme'
-import Footer from "./Footer";
-import Cards from './PostCards';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import AppBar from "./Navbar"
 import App from "./App";
 
 const theme = createMuiTheme({
