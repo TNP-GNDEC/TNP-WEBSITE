edea4ccaee86170cf3326c0304306aaafc0a5f0b
gursidak_singh <gursidak2000@gmail.com>
2020-12-24 01:51:16 +0530

    modified right side nav button

--- resources/js/components/Coordinator/Menu.jsx
+++ resources/js/components/Coordinator/Menu.jsx
@@ -7,12 +7,8 @@ import Popper from '@material-ui/core/Popper';
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
 import { makeStyles } from '@material-ui/core/styles';
-// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-// import { PlayCircleFilledWhite } from '@material-ui/icons';
-import avatar from "../../../images/avatar11.jpeg";
-import Avatar from '@material-ui/core/Avatar';
 
-
+//importing  Icons
 import SettingsIcon from '@material-ui/icons/Settings';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
@@ -40,13 +36,17 @@ const useStyles = makeStyles((theme) => ({
     height: theme.spacing(7),
   },
   menuIcons:{
-    paddingRight:"8px"
+    paddingRight:"8px",
+    fontSize:"28px"
   },
   heading:{
     display:"flex",
     textAlign:"center",
     justifyContent:"center"
 
+  },
+  Icons:{
+    color:'#fff'
   }
 }));
 
@@ -93,8 +93,9 @@ export default function MenuListComposition() {
           aria-haspopup="true"
           onClick={handleToggle}
           style={{ backgroundColor: 'transparent' }} >
-        <Avatar alt="avatar" src={avatar}/>
+          <SettingsIcon fontSize='large' className={classes.Icons} />
         </Button>
+
         <h6 className={classes.heading}>Admin</h6>
         <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
           {({ TransitionProps, placement }) => (
@@ -106,8 +107,8 @@ export default function MenuListComposition() {
                 <ClickAwayListener onClickAway={handleClose}>
                   <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
                     <MenuItem onClick={handleClose}> < SettingsIcon   className={classes.menuIcons}  /> Profile</MenuItem>
-                    <MenuItem onClick={handleClose}> < ExitToAppIcon  className={classes.menuIcons}  /> My account</MenuItem>
-                    <MenuItem onClick={handleClose}> < AccountBoxIcon className={classes.menuIcons}  /> Logout</MenuItem>
+                    <MenuItem onClick={handleClose}> < AccountBoxIcon  className={classes.menuIcons}  /> My account</MenuItem>
+                    <MenuItem onClick={handleClose}> <ExitToAppIcon  className={classes.menuIcons}  /> Logout</MenuItem>
                   </MenuList>
                 </ClickAwayListener>
               </Paper>
@@ -119,98 +120,3 @@ export default function MenuListComposition() {
   );
 }
 
-
-// import React from 'react';
-// import { withStyles } from '@material-ui/core/styles';
-// import Button from '@material-ui/core/Button';
-// import Menu from '@material-ui/core/Menu';
-// import MenuItem from '@material-ui/core/MenuItem';
-// import ListItemIcon from '@material-ui/core/ListItemIcon';
-// import ListItemText from '@material-ui/core/ListItemText';
-// import InboxIcon from '@material-ui/icons/MoveToInbox';
-// import DraftsIcon from '@material-ui/icons/Drafts';
-// import SendIcon from '@material-ui/icons/Send';
-// import logo from '../../../images/logo.png';
-// import Avatar from '@material-ui/core/Avatar';
-
-
-// const StyledMenu = withStyles({
-//   paper: {
-//     border: '1px solid #d3d4d5',
-//   },
-// })((props) => (
-//   <Menu
-//     elevation={0}
-//     getContentAnchorEl={null}
-//     anchorOrigin={{
-//       vertical: 'bottom',
-//       horizontal: 'center',
-//     }}
-//     transformOrigin={{
-//       vertical: 'top',
-//       horizontal: 'center',
-//     }}
-//     {...props}
-//   />
-// ));
-
-// const StyledMenuItem = withStyles((theme) => ({
-//   root: {
-//     '&:focus': {
-//       backgroundColor: theme.palette.primary.main,
-//       '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
-//         color: theme.palette.common.white,
-//       },
-//     },
-//   },
-// }))(MenuItem);
-
-// export default function CustomizedMenus() {
-//   const [anchorEl, setAnchorEl] = React.useState(null);
-
-//   const handleClick = (event) => {
-//     setAnchorEl(event.currentTarget);
-//   };
-
-//   const handleClose = () => {
-//     setAnchorEl(null);
-//   };
-
-//   return (
-//     <div>
-//       <Button
-//         aria-controls="customized-menu"
-//         aria-haspopup="true"
-//         onClick={handleClick}
-//       >
-//         <Avatar alt="Remy Sharp" src={logo} />
-//       </Button>
-//       <StyledMenu
-//         id="customized-menu"
-//         anchorEl={anchorEl}
-//         open={Boolean(anchorEl)}
-//         onClose={handleClose}
-//       > 
-
-//         <StyledMenuItem >
-//           <ListItemIcon>
-//             <SendIcon fontSize="small" />
-//           </ListItemIcon>
-//           <ListItemText primary="Sent mail" />
-//         </StyledMenuItem>
-//         <StyledMenuItem>
-//           <ListItemIcon>
-//             <DraftsIcon fontSize="small" />
-//           </ListItemIcon>
-//           <ListItemText primary="Drafts" />
-//         </StyledMenuItem>
-//         <StyledMenuItem>
-//           <ListItemIcon>
-//             <InboxIcon fontSize="small" />
-//           </ListItemIcon>
-//           <ListItemText primary="Inbox" />
-//         </StyledMenuItem>
-//       </StyledMenu>
-//     </div>
-//   );
-// }
