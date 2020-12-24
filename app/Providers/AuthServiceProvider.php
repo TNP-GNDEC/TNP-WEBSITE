663b50980dafa7ec80f2e49f01e41e1061e661c9
palak199 <p.thapar99@gmail.com>
2020-12-24 15:14:48 +0530

    removed passport

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use Laravel\Passport\Passport;
 use Route;
 
 class AuthServiceProvider extends ServiceProvider
@@ -26,7 +25,6 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-        Passport::routes();
         //
         
     }
