b2e2ca7af039b25e6f2d0696e23e3932d6211b73
gursidak_singh <gursidak2000@gmail.com>
2020-12-29 03:26:30 +0530

    added basic structure required for create post and manage posts place all the content of create posts in createpost.jsx and managePost.jsx is for manage posts

---
+++ resources/js/components/RedirectPages/Coordinator/CreatePost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreatePost() {
+    return (
+        <>
+            <h2>create post content here</h2>
+        </>
+    )
+}
+
+export default CreatePost
---
+++ resources/js/components/RedirectPages/Coordinator/ManagePost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ManagePost() {
+    return (
+        <>
+            <h2>MAnage Post Content here</h2>
+        </>
+    )
+}
+
+export default ManagePost
