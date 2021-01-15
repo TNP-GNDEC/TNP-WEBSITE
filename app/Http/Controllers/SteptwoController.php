9fd2e03f39fd737657eef817b19736333f0e3251
palak199 <p.thapar99@gmail.com>
2021-01-15 22:55:08 +0530

    changes to post request form 2

--- app/Http/Controllers/SteptwoController.php
+++ app/Http/Controllers/SteptwoController.php
@@ -25,10 +25,6 @@ class SteptwoController extends Controller
  public function getFormData(Request $request)
   {
     $user = User::findOrFail($request->id);
-  //   $request->validate([
-  //     'username' => 'required', 
-  //     'password' => 'required|min:6'
-  // ]);
   $current_step= DB::table('form_statuses')
                     ->where('user_id', $user->id)
                     ->value('form_step');
@@ -36,36 +32,40 @@ class SteptwoController extends Controller
   $details = DB::table('step_two')
   ->where('user_id', $user->id)
   ->update([
-      'first_name' => $request->first_name, 
-      'last_name' => $request->last_name,
+      'first_name' => $request->profile->first_name, 
+      'last_name' => $request->profile->last_name,
+      'height' => $request->profile->height,
+      'weight' => $request->profile->weight,
+      'dob' => $request->profile->dob,
+      'blood_group' => $request->profile->blood_group,
       'user_id' => $request->id,
-      'mother_name' => $request->mother_name,
-      'father_name' => $request->father_name,
-      'father_mobile' => $request->father_contact,
-      'mother_mobile' => $request->mother_contact,
-      'dob' => $request->dob,
-      'height' => $request->height,
-      'weight' => $request->weight,
-      'hostler'=>$request->hostler,
-      'mobile' => $request->mobile,
-      'whatsapp' => $request->whatsapp,
-      'leet' => $request->leet,
-      'category' => $request->category,
-      'disability' => $request->disability,
-      'shift' => $request->shift,
-      'stream' => $request->stream,
-      'branch_type' => $request->branch_type,
-      'training_sem' => $request->training_sem,
-      'blood_group' => $request->blood_group,
-      'marital_status' => $request->marital_status,
-      'aadhar' => $request->aadhar,
-      'farming_background' => $request->farming_background,
-      'pincode' => $request->pincode,
-      'district' => $request->district,
-      'city' => $request->city,
-      'state' => $request->state,
-      'address' => $request->address,
-      'gender' => $request->gender
+      'gender' => $request->profile->gender,
+      'marital_status' => $request->profile->marital_status,
+      'disability' => $request->profile->disability,
+      'aadhar' => $request->profile->aadhar,
+
+      'mother_name' => $request->parents->mother_name,
+      'father_name' => $request->parents->father_name,
+      'father_mobile' => $request->parents->father_phone,
+      'mother_mobile' => $request->parents->mother_phone,
+      'farming_background' => $request->parents->farming_background,
+      
+      'shift' => $request->academics->shift,
+      'stream' => $request->academics->stream,
+      'category' => $request->academics->category,
+      'branch_type' => $request->academics->branch_type,
+      'training_sem' => $request->academics->training_sem,
+      'hostler' => $request->academics->hostler,
+      'leet' => $request->academics->leet,
+
+      'mobile' => $request->contact->mobile,
+      'whatsapp' => $request->contact->whatsapp,
+      
+      'pincode' => $request->address->pincode,
+      'district' => $request->address->district,
+      'city' => $request->address->city,
+      'state' => $request->address->state,
+      'address' => $request->address->address,
 
   ]);
     $form_step_change= DB::table('form_statuses')
