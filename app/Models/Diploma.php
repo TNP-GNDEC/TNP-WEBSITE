78a15178d5a99778d1879d5cc1abd5f8e4e99855
palak199 <p.thapar99@gmail.com>
2021-01-25 23:23:46 +0530

    added models controllers for 12th diploma

---
+++ app/Models/Diploma.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Diploma extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "board",
+        "institution_name",
+        "city_of_institute",
+        "state_of_institute",
+        "year_of_passing:",
+        "obtained_marks:",
+        "maximum_marks: ",
+        "stream",
+        "file",
+        "user_id"
+    ];
+}
---
+++ app/Models/Twelfth.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Twelfth extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        "board",
+        "institution_name",
+        "jee_rank",
+        "city_of_institute",
+        "state_of_institute",
+        "year_of_passing",
+        "obtained_marks",
+        "maximum_marks",
+        "file"
+    ];
+}
