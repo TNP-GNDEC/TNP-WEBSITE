51aff3739131808a7c5efdd3b5d30f3127d68349
SuryanshChopra <suryanshchopra987@gmail.com>
2021-02-16 17:40:02 +0530

    Added Tabs to Users.jsx

--- resources/js/components/RedirectPages/Coordinator/Users.jsx
+++ resources/js/components/RedirectPages/Coordinator/Users.jsx
@@ -1,64 +1,94 @@
 import React from 'react';
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
-import Button from "@material-ui/core/Button";
-import { Container } from "@material-ui/core";
+import PropTypes from 'prop-types';
+import SwipeableViews from 'react-swipeable-views';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        backgroundColor: theme.palette.background.paper,
-        width: "100%"
-    },
-    heading: {
-        padding: "20px 0px",
-        letterSpacing: "0.6px",
-        wordSpacing: "8px",
-        color: theme.palette.primary.dark
-    },
-    tabBody: {
-        marginTop: "40px"
-    }
-}));
-
-function Users() {
-
-    const classes = useStyles();
-    const [heading, setHeading] = React.useState("CREATE USERS");
-
-    const renderPostTab = () => {
-        if (heading == "CREATE POSTS") {
-            return <> hello </>;
-        } else if (heading == "MANAGE POSTS") {
-            return <>hey</>;
-        }
-    };
-    return (
-        <>
-         <h2 className={classes.heading}>{heading}</h2>
-                    <ButtonGroup
-                        size="large"
-                        color="primary"
-                        aria-label="large outlined primary button group"
-                    >
-                        <Button
-                            onClick={() => {
-                                setHeading("CREATE USERS");
-                            }}
-                        >
-                            CREATE POSTS
-                        </Button>
-                        <Button
-                            onClick={() => {
-                                setHeading("MANAGE USERS");
-                            }}
-                        >
-                            MANAGE POSTS
-                        </Button>
-                    </ButtonGroup>
-                    {renderPostTab()}
-    </>
-    )
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
 }
 
-export default Users
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    width: 500,
+  },
+}));
+
+export default function FullWidthTabs() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab label="Create Users" {...a11yProps(0)} />
+          <Tab label="Manage Users" {...a11yProps(1)} />
+
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          Create Users
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          Manage Users
+        </TabPanel>
+      </SwipeableViews>
+    </div>
+  );
+}
