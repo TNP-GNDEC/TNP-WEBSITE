fa55e77eb53e0047094b905a0f9571741dfb4af5
vikalp2502 <vikalpkaushik97@gmail.com>
2021-07-21 14:35:59 +0530

    added connect card

--- resources/js/components/HomeComponent/LeftBar/Connect.js
+++ resources/js/components/HomeComponent/LeftBar/Connect.js
@@ -1,23 +1,26 @@
-import React from "react";
-import Axios from "axios";
-import ConnectData from "./LeftBarData/ConnectData";
-import { withStyles } from '@material-ui/core/styles';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
-import Person from '@material-ui/icons/PersonAdd';
-import Loading from "../SideComponents/LoadingCard";
+import Business from '@material-ui/icons/Business';
+import Email from '@material-ui/icons/Email';
+import Phone from '@material-ui/icons/Phone';
 
-const useStyles = theme => ({
-    root: {
-      width: "100%",
-      padding: "10px",
-      marginBottom: "20px",
-      boxShadow: "0 1px 1px rgba(0,0,0,0.15),0 8px 0 -5px #eee,0 8px 1px -4px rgba(0,0,0,0.15),0 16px 0 -10px #eee,0 16px 1px -9px rgba(0,0,0,0.15)",
-    },
-    header:{
-        display: "flex",
-    },
-    icon:{
+import whatsapp from "../../../../images/whatsapp.png";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    paddingTop: "15px",
+    marginBottom: "20px",
+    textAlign: "center",
+    borderRadius: "16px",
+    boxShadow: "0px 15px 30px #8a959e33",
+  },
+  media:{
+    textAlign: "center"
+  },
+  icon:{
       backgroundColor: theme.palette.primary.dark,
       color: theme.palette.secondary.main,
       borderRadius: "5px",
@@ -32,57 +35,90 @@ const useStyles = theme => ({
   Icon:{
     fontSize: "18px",
   },
-    title:{
-        color: theme.palette.primary.dark,
-        fontSize: "22px"
+  title:{
+      color: theme.palette.primary.dark,
+      fontSize: "22px"
+  },
+  imageBox:{
+      marginLeft: "auto",
+      marginRight: "auto",
+      width: "50px",
+      height: "50px",
+      borderRadius: "50%",
+      backgroundColor: "#25D366",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+  },
+  image:{
+      width: "30px",
+      height: "30px",
+  },
+  heading:{
+    fontSize: "18px",
+    fontFamily: "Open Sans",
+    fontWeight: "600",
+    color: theme.palette.primary.dark,
+    paddingTop: "5px"
+  },
+  subheading:{
+      fontSize: "13px",
+      fontFamily: "Open Sans",
+      fontWeight: "600",
+      color: theme.palette.primary.text,
+  },
+  button:{
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginBottom: "20px",
+    marginTop: "20px",
+    width: "80%",
+    height: "40px",
+    cursor: "pointer",
+    borderRadius: "8px",
+    border: "2px solid #1687d9",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    "&:hover": {
+      boxShadow: "0 5px 12px #1687d933",
     },
-});
+    "&:focus": {
+      outline: "none",
+    },
+},
+subheading2:{
+    fontSize: "14px",
+    fontFamily: "Open Sans",
+    fontWeight: "600",
+    color: theme.palette.primary.main,
+},
+}));
 
-class Connect extends React.Component {
-    state = {
-        data: [],
-        loading: true
-    }
-    fetchData = async () => {
-        const res = await Axios.get("/addConnectItems");
-        if(res.data.status === 200){
-            this.setState({data: res.data.data});
-            this.setState({loading: false});
-        }
-    }
-    componentDidMount(){
-        this.fetchData();
-    }
+export default function Office() {
+  const classes = useStyles();
 
-    deleteData = async (id) => {
-        const res = await Axios.delete(`/addConnectItems/${id}`);
-        if(res.data.status === 200){
-            this.fetchData();
-        }
-    }
-
-    render(){
-        if(this.state.loading){
-            return <Loading />
-        }
-        if(Object.keys(this.state.data).length == 0){
-            return <div></div>
-        }
-        const {classes} = this.props;
-        return(
-            <Card className={classes.root} variant="outlined">
-                <div className={classes.header}>
-                <div className={classes.icon}><Person className={classes.Icon}/></div>
-                    <Typography variant="h5" component="h2" className={classes.title}>
-                        Connect
-                    </Typography>
-                </div>
-                {this.state.data.map(data => (
-                    <ConnectData data = {data} key={data.id} deleteData = {this.deleteData}/>
-                ))}
-            </Card>
-        )
-    }
+  return (
+    <Card className={classes.root}>
+      <div className={classes.media}>
+        <div className={classes.imageBox}>
+            <img src={whatsapp} className={classes.image}/>
+        </div>
+      </div>
+      <h1 className={classes.heading}>
+        Connect with the TPO
+      </h1>
+      <h3 className={classes.subheading}>
+        via Whatsapp
+      </h3>
+      <a href="https://api.whatsapp.com/send?phone=919915794339" target="_blank" rel="noopener"
+      style={{ textDecoration: 'none' }}>
+      <div className={classes.button}>
+        <Typography variant="h6" component="h2" className={classes.subheading2}>
+          Send Message
+        </Typography>
+      </div> 
+      </a>  
+    </Card>
+  );
 }
-
-export default withStyles(useStyles)(Connect);
