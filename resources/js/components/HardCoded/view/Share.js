0267c4f8306e44c0cf871d3e95efdb798fe989ae
vikalp2502 <vikalpkaushik97@gmail.com>
2021-07-28 15:38:26 +0530

    share post option

---
+++ resources/js/components/HardCoded/view/Share.js
@@ -0,0 +1,166 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import Copy from '@material-ui/icons/FilterNone';
+import Copied from '@material-ui/icons/LibraryAddCheck';
+import { CopyToClipboard} from 'react-copy-to-clipboard';
+import {FacebookShareButton, FacebookIcon,
+WhatsappShareButton, WhatsappIcon,
+LinkedinShareButton, LinkedinIcon,
+TwitterShareButton,TwitterIcon,
+EmailShareButton,EmailIcon} from 'react-share';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    paddingTop: "15px",
+    marginBottom: "20px",
+    textAlign: "center",
+    borderRadius: "16px",
+    boxShadow: "0px 15px 30px #8a959e33",
+    ['@media (min-width:1600px)']: {
+      paddingTop: "20px"
+  },
+  },
+  heading:{
+    fontSize: "18px",
+    fontFamily: "Open Sans",
+    fontWeight: "600",
+    color: theme.palette.primary.dark,
+    paddingTop: "5px",
+    ['@media (min-width:1600px)']: {
+      fontSize: "20px",
+      paddingTop: "12px"
+    },
+  },
+  subheading:{
+      fontSize: "13px",
+      fontFamily: "Open Sans",
+      fontWeight: "600",
+      color: theme.palette.primary.text,
+      ['@media (min-width:1600px)']: {
+        fontSize: "16px"
+    },
+  },
+  subheading2:{
+    fontSize: "14px",
+    fontFamily: "Open Sans",
+    fontWeight: "600",
+    color: theme.palette.primary.main,
+    ['@media (min-width:1600px)']: {
+      fontSize: "16px"
+    },
+  },
+  linkDiv:{
+    width: "90%",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  mediaDiv:{
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    paddingTop: "10px",
+    marginBottom: "15px"
+  },
+  Button:{
+    width: "100%",
+  },
+  Icon: {
+    width: "34px",
+    height: "34px",
+    borderRadius: "50px",
+    ['@media (min-width:1600px)']: {
+      width: "42px",
+      height: "42px"
+    },
+  },
+  linkDiv2:{
+    width: "100%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    borderRadius: "12px",
+    height: "50px",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    background: theme.palette.primary.accent,
+    color: theme.palette.primary.main,
+    marginBottom: "20px"
+  },
+  paraDiv:{
+    width: "75%",
+    paddingLeft: "15px",
+    paddingTop: "15px",
+    textAlign: "left"
+  },
+  iconDiv:{
+    width: "20%",
+  },
+  para:{
+    wordBreak: "break-all",
+    lineHeight: "20px",
+    fontSize: "14px",
+    fontFamily: "Open Sans",
+    fontWeight: "400",
+    ['@media (min-width:1600px)']: {
+      fontSize: "16px"
+    },
+  },
+  icon2:{
+    height: "30px",
+    ['@media (min-width:1600px)']: {
+      height: "40px"
+    },
+  },
+}));
+
+export default function Share(props) {
+  const classes = useStyles();
+  const [click, setClick] = React.useState(false);
+  const handleClick = () => {
+    setClick(true);
+  }
+  const {id} = props;
+  const shareLink = "https://tnpgndec.com/showPost/" + id;
+  return (
+    <Card className={classes.root}>
+      <h1 className={classes.heading}>
+        Share this post
+      </h1>
+      <h3 className={classes.subheading}>
+        via Social Media
+      </h3>
+      <div className={classes.linkDiv}>
+        <div className={classes.mediaDiv}>
+          <FacebookShareButton className={classes.Button} url={shareLink}>
+            <FacebookIcon className={classes.Icon} />
+          </FacebookShareButton>
+          <WhatsappShareButton className={classes.Button} url={shareLink}>
+            <WhatsappIcon className={classes.Icon} />
+          </WhatsappShareButton>
+          <LinkedinShareButton className={classes.Button} url={shareLink}>
+            <LinkedinIcon className={classes.Icon} />
+          </LinkedinShareButton>
+          <TwitterShareButton className={classes.Button} url={shareLink}>
+            <TwitterIcon className={classes.Icon} />
+          </TwitterShareButton>
+          <EmailShareButton className={classes.Button} url={shareLink}>
+            <EmailIcon className={classes.Icon} />
+          </EmailShareButton>
+        </div>
+        <div className={classes.linkDiv2}>
+          <div className={classes.paraDiv}>
+          <p className={classes.para}>{shareLink}</p>
+          </div>
+          <div className={classes.iconDiv}>
+            <CopyToClipboard text={shareLink} onCopy={handleClick}>
+              {click ? <Copied className={classes.icon2} /> :<Copy className={classes.icon2} />}
+            </CopyToClipboard>
+          </div>
+        </div>
+      </div> 
+    </Card>
+  );
+}
--- resources/js/components/HardCoded/view/ShowPost.js
+++ resources/js/components/HardCoded/view/ShowPost.js
@@ -1,6 +1,7 @@
 import React, { useState , useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import moment from "moment";
+import Share from "./Share";
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
@@ -45,20 +46,20 @@ const useStyles = makeStyles((theme) => ({
     top: "60px"
   },
   left:{
-    marginTop: "40px",
+    marginTop: "60px",
     width: "100%",
     ['@media (max-width:960px)']: {
       display: "none",
    }
   },
   center:{
-    marginTop: "40px",
+    marginTop: "60px",
     ['@media (max-width:960px)']: {
       width: "100%",
    }
   },
   right:{
-    marginTop: "40px",
+    marginTop: "60px",
     width: "100%",
     ['@media (max-width:960px)']: {
        display: "none",
@@ -79,157 +80,235 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: "30px",
     borderRadius: "16px",
     boxShadow: "0px 15px 30px #8a959e33",
+    ['@media (min-width:1600px)']: {
+            padding: "20px"
+        },
 },
 type:{
-    padding: "3px",
-    backgroundColor: theme.palette.secondary.accent,
-    fontSize: "12px",
-    width: "30%",
-    textAlign: "center",
-    borderRadius: "8px",
-    color: theme.palette.primary.dark,
-    fontFamily: "Open Sans",
-    fontWeight: "600",
-    ['@media (max-width:960px)']: {
-        fontSize: "10px",
-        width: "40%"
-    },
+  padding: "3px",
+  backgroundColor: theme.palette.secondary.accent,
+  fontSize: "12px",
+  width: "30%",
+  textAlign: "center",
+  borderRadius: "8px",
+  color: theme.palette.primary.dark,
+  fontFamily: "Open Sans",
+  fontWeight: "600",
+  ['@media (max-width:960px)']: {
+      fontSize: "10px",
+      width: "40%"
+  },
+  ['@media (min-width:1600px)']: {
+      fontSize: "16px",
+      width: "30%"
+  },
 },
 header: {
-    width: "100%",
-    display: "flex"
+  width: "100%",
+  display: "flex"
 },
 header2: {
-    width: "100%",
-    paddingTop: "5px"
+  width: "100%",
+  paddingTop: "5px"
 },
 image: {
-    width: "14px",
-    height: "14px",
-    ['@media (max-width:960px)']: {
-        width: "13px",
-        height: "13px",
-    },
+  width: "14px",
+  height: "14px",
+  ['@media (max-width:960px)']: {
+      width: "13px",
+      height: "13px",
+  },
+  ['@media (min-width:1600px)']: {
+      width: "17px",
+      height: "17px",
+  },
 },
 title: {
-    fontSize: "24px",
-    textTransform: "uppercase",
-    wordWarp: "word-break",
-    fontFamily: "Open Sans",
-    fontWeight: "600",
-    color: theme.palette.primary.dark,
-    ['@media (max-width:960px)']: {
-        fontSize: "18px",
-    },
+  fontSize: "24px",
+  textTransform: "uppercase",
+  wordWarp: "word-break",
+  fontFamily: "Open Sans",
+  fontWeight: "600",
+  color: theme.palette.primary.dark,
+  ['@media (max-width:960px)']: {
+      fontSize: "18px",
+  },
+  ['@media (min-width:1600px)']: {
+      fontSize: "32px",
+  },
 },
 subheader: {
-    display: "flex",
-    justifyContent: "space-between",
-    padding: "5px 0px 0px 0px"
+  display: "flex",
+  justifyContent: "space-between",
+  padding: "5px 0px 0px 0px"
 },
 subheader2: {
-    display: "flex",
-    justifyContent: "space-between",
-    padding: "0px 5px 0px 0px",
+  display: "flex",
+  justifyContent: "space-between",
+  padding: "0px 5px 0px 0px",
 },
 subheader3: {
-    display: "flex",
-    padding: "0px",
-    width: "10%",
-    justifyContent: "space-between",
-    alignItems: "flex-start",
-    ['@media (max-width:600px)']: {
-        width: "20%",
-    },
+  display: "flex",
+  padding: "0px",
+  width: "10%",
+  justifyContent: "space-between",
+  alignItems: "flex-start",
+  ['@media (max-width:600px)']: {
+      width: "20%",
+  },
 },
 subheading: {
-    fontSize: "12px",
-    fontFamily: "Open Sans",
-    fontWeight: "600",
-    paddingLeft: "5px",
-    color: theme.palette.primary.dark,
-    ['@media (max-width:960px)']: {
-        fontSize: "10px",
-    },
+  fontSize: "12px",
+  fontFamily: "Open Sans",
+  fontWeight: "600",
+  paddingLeft: "5px",
+  color: theme.palette.primary.dark,
+  ['@media (max-width:960px)']: {
+      fontSize: "10px",
+  },
+  ['@media (min-width:1600px)']: {
+      fontSize: "16px",
+  },
 },
 icon: {
-    fontSize: "18px"
+  fontSize: "18px"
 },
 subheading2: {
-    fontSize: "16px !important",
-    color: theme.palette.primary.text,
-    padding: "30px 0px",
-    textAlign: "justify",
-    fontFamily: "Open Sans !important",
-    ['@media (max-width:960px)']: {
-        fontSize: "13px !important",
-    },
+  fontSize: "16px !important",
+  color: theme.palette.primary.text,
+  padding: "30px 0px",
+  textAlign: "justify",
+  fontFamily: "Open Sans !important",
+  ['@media (max-width:960px)']: {
+      fontSize: "13px !important",
+  },
 },
 subheading3: {
-    fontSize: "16px",
-    paddingLeft: "5px"
+  fontSize: "16px",
+  paddingLeft: "5px"
 },
 subheading4: {
-    fontSize: "16px",
+  fontSize: "16px",
 },
 read: {
-    borderRadius: "5px",
-    width: "100%",
-    margin: "8px auto",
-    height: "40px",
-    marginTop: "5px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: "10px 0px 0px 0px"
+  borderRadius: "5px",
+  width: "100%",
+  margin: "8px auto",
+  height: "40px",
+  marginTop: "5px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "10px 0px 0px 0px"
 },
 readLink: {
-    width: "100%",
-    textDecoration: "none"
+  width: "100%",
+  textDecoration: "none"
 },
 readButton: {
-    backgroundColor: theme.palette.primary.main,
-    padding: "10px 0px",
-    fontSize: "16px",
-    borderRadius: "16px",
-    fontFamily: "Open Sans",
-    fontWeight: "400",
-    textTransform: "capitalize",
-    boxShadow: "0px 10px 25px #1687d933",
-    color: theme.palette.secondary.main,
-    textDecoration: "none",
-    '&:hover': {
-        backgroundColor: theme.palette.primary.main,
-    },
-    '&:focus': {
-        outline: "none"
-    },
-    ['@media (max-width:960px)']: {
-        fontSize: "14px",
-    },
+  backgroundColor: theme.palette.primary.main,
+  padding: "10px 0px",
+  fontSize: "16px",
+  borderRadius: "16px",
+  fontFamily: "Open Sans",
+  fontWeight: "400",
+  textTransform: "capitalize",
+  boxShadow: "0px 10px 25px #1687d933",
+  color: theme.palette.secondary.main,
+  textDecoration: "none",
+  '&:hover': {
+      backgroundColor: theme.palette.primary.main,
+  },
+  '&:focus': {
+      outline: "none"
+  },
+  ['@media (max-width:960px)']: {
+      fontSize: "14px",
+  },
+  ['@media (min-width:1600px)']: {
+      fontSize: "18px",
+  },
 },
 body2: {
-    display: "flex",
-    justifyContent: "space-between"
+  display: "flex",
+  justifyContent: "space-between"
 },
 Icons: {
-    width: "16px",
-    height: "16px",
-    padding: "0px",
-    color: theme.palette.primary.dark,
-    verticalAlign: "initial",
-    ['@media (max-width:960px)']: {
-        width: "14px",
-        height: "14px",
-    },
+  width: "16px",
+  height: "16px",
+  padding: "0px",
+  color: theme.palette.primary.dark,
+  verticalAlign: "initial",
+  ['@media (max-width:960px)']: {
+      width: "14px",
+      height: "14px",
+  },
+  ['@media (min-width:1600px)']: {
+      width: "20px",
+      height: "20px",
+  },
 },
-tagslist:{
-  width: "100%",
-  marginTop: "5px",
-  marginBottom: "10px",
+Tags: {
+  backgroundColor: "#3b59981a",
+  color: theme.palette.primary.dark,
+  width: "40%",
+  height: "40px",
+  borderRadius: "5px",
   display: "flex",
-  justifyContent: "start",
+  padding: "0 10px",
   alignItems: "center",
+},
+socialIcons: {
+  display: "flex",
+  padding: "0px 20px"
+},
+socialIcon: {
+  borderRadius: "5px",
+  backgroundColor: "#3b59981a",
+  color: "#3b5998",
+  marginRight: "8px",
+  width: "40px",
+  height: "40px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+},
+socialIcon2: {
+  borderRadius: "5px",
+  backgroundColor: "#0077b51a ",
+  // border: "1px solid #0077b5",
+  // boxShadow: "0px 15px 25px #0077b51a",
+  color: "#0077b5",
+  marginRight: "8px",
+  width: "40px",
+  height: "40px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+},
+socialIcon3: {
+  borderRadius: "5px",
+  backgroundColor: "#00acee1a",
+  color: "#00acee ",
+  marginRight: "8px",
+  width: "40px",
+  height: "40px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+},
+iconLinks: {
+  textDecoration: "none",
+  color: "#fff",
+},
+body3: {
+  maxHeight: "250px",
+  overflow: "Hidden",
+},
+pdfs: {
+  width: "70%",
+  height: "500px",
+  margin: "auto",
 },
 tag: {
     backgroundColor: theme.palette.secondary.accent,
@@ -285,11 +364,15 @@ export default function ShowPost() {
       <Navbar />
       <div className={classes.root}>
         <Scroll showBelow={250} />
+
         <Grid container spacing={3}>
           <Grid item md={3} className={classes.left} display={{ xs: 'none', md: 'block' }}>
             <Paper className={classes.paper}>
+              <h3 className={classes.cardTitle}>Share</h3>
+              <Share id={data.posts.id} />
             </Paper>
           </Grid>
+
           <Grid item md={6} className={classes.center}>
             <Paper className={classes.paper}>
             <h3 className={classes.cardTitle}>Full Post</h3>
