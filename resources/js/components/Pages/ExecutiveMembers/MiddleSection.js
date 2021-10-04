cc0f1b0bac44366e53fff1b547a4a6657b91631f
Avninder Preet Singh <53931646+Avninder99@users.noreply.github.com>
2021-10-04 20:28:27 +0530

    Updated dropdown and links in executive members page

--- resources/js/components/Pages/ExecutiveMembers/MiddleSection.js
+++ resources/js/components/Pages/ExecutiveMembers/MiddleSection.js
@@ -21,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
   formControl: {
     backgroundColor: "#FFFFFF",
     borderRadius: "16px",
-    border: "3px #B1B1B1 solid",
+    border: "1px #B1B1B1 solid",
     overflow: "hidden",
     paddingLeft: "15px",
     paddingRight: "15px",
@@ -77,6 +77,17 @@ export default function MiddleSection() {
       <h3 className={classes.cardTitle} style={{ marginBottom: 15 }}>Executive Members</h3>
       <FormControl variant="standard" className={classes.formControl} fullWidth>
         <Select
+          MenuProps={{
+            anchorOrigin: {
+              vertical: "bottom",
+              horizontal: "left"
+            },
+            transformOrigin: {
+              vertical: "top",
+              horizontal: "left"
+            },
+            getContentAnchorEl: null
+          }}
           labelId="demo-simple-select-label"
           id="demo-simple-select"
           value={index}
@@ -85,10 +96,10 @@ export default function MiddleSection() {
           style={{padding: 5, fontSize: 20}}
           disableUnderline
         >
-          <MenuItem value={0} style={{fontSize: 18}}>2018-19</MenuItem>
-          <MenuItem value={1} style={{fontSize: 18}}>2019-20</MenuItem>
-          <MenuItem value={2} style={{fontSize: 18}}>2020-21</MenuItem>
           <MenuItem value={3} style={{fontSize: 18}}>2021-22</MenuItem>
+          <MenuItem value={2} style={{fontSize: 18}}>2020-21</MenuItem>
+          <MenuItem value={1} style={{fontSize: 18}}>2019-20</MenuItem>
+          <MenuItem value={0} style={{fontSize: 18}}>2018-19</MenuItem>
         </Select>
       </FormControl>
       <Card className={classes.pdfCard}>
