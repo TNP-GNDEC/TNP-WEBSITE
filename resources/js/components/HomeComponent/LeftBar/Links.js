42336f27525c0c7f13eb3a3769c6ba5d182fcb67
Prabhkirat Singh <prabhkiratsingh65@gmail.com>
2023-07-27 14:37:06 +0530

    Updated Core Team Details and Links

--- resources/js/components/HomeComponent/LeftBar/Links.js
+++ resources/js/components/HomeComponent/LeftBar/Links.js
@@ -1,24 +1,25 @@
 import React from "react";
 import Axios from "axios";
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/icons/Link';
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/icons/Link";
 import { Icon } from "@material-ui/core";
-import gneLogo from '../../../../images/gndec.png';
-import linkIcon from '../../../../images/Link.png';
-import naukriIcon from '../../../../images/naukri-com.png';
-import bixIcon from '../../../../images/indiabix.jpg';
-import indeedIcon from '../../../../images/indeed.jpg';
-import internshalaIcon from '../../../../images/internshala.jpg';
+import gneLogo from "../../../../images/gndec.png";
+import linkIcon from "../../../../images/Link.png";
+import naukriIcon from "../../../../images/naukri-com.png";
+import bixIcon from "../../../../images/indiabix.jpg";
+import indeedIcon from "../../../../images/indeed.jpg";
+import internshalaIcon from "../../../../images/internshala.jpg";
+import masterquizIcon from "../../../../images/masterquiz.png";
 
 const useStyles = theme => ({
-    title:{
+    title: {
         color: theme.palette.primary.dark,
         fontSize: "20px",
         fontWeight: "500"
     },
-    card:{
+    card: {
         width: "100%",
         height: "56px",
         padding: "8px",
@@ -30,7 +31,7 @@ const useStyles = theme => ({
             padding: "12px"
         }
     },
-    cardTitle:{
+    cardTitle: {
         fontSize: "16px",
         fontWeight: "600",
         fontFamily: "Open Sans",
@@ -43,14 +44,14 @@ const useStyles = theme => ({
             marginLeft: "30px"
         }
     },
-    container:{
+    container: {
         display: "flex"
     },
-    icon:{
+    icon: {
         width: "40px",
         height: "40px"
     },
-    linkIcon:{
+    linkIcon: {
         width: "20px",
         height: "20px",
         marginTop: "auto",
@@ -63,7 +64,7 @@ const useStyles = theme => ({
     },
     outerContainer: {
         display: "flex",
-        justifyContent: "space-between",
+        justifyContent: "space-between"
     },
     link: {
         marginTop: "auto",
@@ -72,92 +73,160 @@ const useStyles = theme => ({
 });
 
 class Links extends React.Component {
-
-    render(){
-        const {classes} = this.props;
-        return(
+    render() {
+        const { classes } = this.props;
+        return (
             <>
-            <Card className={classes.card}>
-                <div className={classes.outerContainer}>
-                    <div className={classes.container}>
-                        <img className={classes.icon} src={gneLogo}></img>
-                        <Typography className={classes.cardTitle}>
-                            GNDEC
-                        </Typography>
+                <Card className={classes.card}>
+                    <div className={classes.outerContainer}>
+                        <div className={classes.container}>
+                            <img className={classes.icon} src={gneLogo}></img>
+                            <Typography className={classes.cardTitle}>
+                                GNDEC
+                            </Typography>
+                        </div>
+                        <div className={classes.link}>
+                            <a
+                                href="https://gndec.ac.in"
+                                target="_blank"
+                                rel="noreferrer noopener"
+                            >
+                                <img
+                                    className={classes.linkIcon}
+                                    src={linkIcon}
+                                ></img>
+                            </a>
+                        </div>
                     </div>
-                    <div className={classes.link}>
-                        <a href="https://gndec.ac.in" target="_blank" rel="noreferrer noopener">
-                            <img className={classes.linkIcon} src={linkIcon}></img>
-                        </a>
-                    </div>
-                </div>
-            </Card>
+                </Card>
 
-            <Card className={classes.card}>
-                <div className={classes.outerContainer}>
-                    <div className={classes.container}>
-                        <img className={classes.icon} src={naukriIcon}></img>
-                        <Typography className={classes.cardTitle}>
-                            Naukri.com
-                        </Typography>
+                <Card className={classes.card}>
+                    <div className={classes.outerContainer}>
+                        <div className={classes.container}>
+                            <img
+                                className={classes.icon}
+                                src={naukriIcon}
+                            ></img>
+                            <Typography className={classes.cardTitle}>
+                                Naukri.com
+                            </Typography>
+                        </div>
+                        <div className={classes.link}>
+                            <a
+                                href="https://naukri.com"
+                                target="_blank"
+                                rel="noreferrer noopener"
+                            >
+                                <img
+                                    className={classes.linkIcon}
+                                    src={linkIcon}
+                                ></img>
+                            </a>
+                        </div>
                     </div>
-                    <div className={classes.link}>
-                        <a href="https://naukri.com" target="_blank" rel="noreferrer noopener">
-                            <img className={classes.linkIcon} src={linkIcon}></img>
-                        </a>
-                    </div>
-                </div>
-            </Card>
+                </Card>
 
-            <Card className={classes.card}>
-                <div className={classes.outerContainer}>
-                    <div className={classes.container}>
-                        <img className={classes.icon} src={bixIcon}></img>
-                        <Typography className={classes.cardTitle}>
-                            IndiaBix
-                        </Typography>
+                <Card className={classes.card}>
+                    <div className={classes.outerContainer}>
+                        <div className={classes.container}>
+                            <img className={classes.icon} src={bixIcon}></img>
+                            <Typography className={classes.cardTitle}>
+                                IndiaBix
+                            </Typography>
+                        </div>
+                        <div className={classes.link}>
+                            <a
+                                href="https://indiabix.com"
+                                target="_blank"
+                                rel="noreferrer noopener"
+                            >
+                                <img
+                                    className={classes.linkIcon}
+                                    src={linkIcon}
+                                ></img>
+                            </a>
+                        </div>
                     </div>
-                    <div className={classes.link}>
-                        <a href="https://indiabix.com" target="_blank" rel="noreferrer noopener">
-                            <img className={classes.linkIcon} src={linkIcon}></img>
-                        </a>
-                    </div>
-                </div>
-            </Card>
+                </Card>
 
-            <Card className={classes.card}>
-                <div className={classes.outerContainer}>
-                    <div className={classes.container}>
-                        <img className={classes.icon} src={indeedIcon}></img>
-                        <Typography className={classes.cardTitle}>
-                            Indeed
-                        </Typography>
+                <Card className={classes.card}>
+                    <div className={classes.outerContainer}>
+                        <div className={classes.container}>
+                            <img
+                                className={classes.icon}
+                                src={indeedIcon}
+                            ></img>
+                            <Typography className={classes.cardTitle}>
+                                Indeed
+                            </Typography>
+                        </div>
+                        <div className={classes.link}>
+                            <a
+                                href="https://indeed.com"
+                                target="_blank"
+                                rel="noreferrer noopener"
+                            >
+                                <img
+                                    className={classes.linkIcon}
+                                    src={linkIcon}
+                                ></img>
+                            </a>
+                        </div>
                     </div>
-                    <div className={classes.link}>
-                        <a href="https://indeed.com" target="_blank" rel="noreferrer noopener">
-                            <img className={classes.linkIcon} src={linkIcon}></img>
-                        </a>
-                    </div>
-                </div>
-            </Card>
+                </Card>
 
-            <Card className={classes.card}>
-                <div className={classes.outerContainer}>
-                    <div className={classes.container}>
-                        <img className={classes.icon} src={internshalaIcon}></img>
-                        <Typography className={classes.cardTitle}>
-                            Internshala
-                        </Typography>
+                <Card className={classes.card}>
+                    <div className={classes.outerContainer}>
+                        <div className={classes.container}>
+                            <img
+                                className={classes.icon}
+                                src={internshalaIcon}
+                            ></img>
+                            <Typography className={classes.cardTitle}>
+                                Internshala
+                            </Typography>
+                        </div>
+                        <div className={classes.link}>
+                            <a
+                                href="https://internshala.com"
+                                target="_blank"
+                                rel="noreferrer noopener"
+                            >
+                                <img
+                                    className={classes.linkIcon}
+                                    src={linkIcon}
+                                ></img>
+                            </a>
+                        </div>
                     </div>
-                    <div className={classes.link}>
-                        <a href="https://internshala.com" target="_blank" rel="noreferrer noopener">
-                            <img className={classes.linkIcon} src={linkIcon}></img>
-                        </a>
+                </Card>
+                <Card className={classes.card}>
+                    <div className={classes.outerContainer}>
+                        <div className={classes.container}>
+                            <img
+                                className={classes.icon}
+                                src={masterquizIcon}
+                            ></img>
+                            <Typography className={classes.cardTitle}>
+                                MasterQuiz
+                            </Typography>
+                        </div>
+                        <div className={classes.link}>
+                            <a
+                                href="https://masterquiz.in"
+                                target="_blank"
+                                rel="noreferrer noopener"
+                            >
+                                <img
+                                    className={classes.linkIcon}
+                                    src={linkIcon}
+                                ></img>
+                            </a>
+                        </div>
                     </div>
-                </div>
-            </Card>
+                </Card>
             </>
-        )
+        );
     }
 }
 
--- resources/js/components/Pages/ExecutiveMembers/MiddleSection.js
+++ resources/js/components/Pages/ExecutiveMembers/MiddleSection.js
@@ -1,80 +1,80 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
 import Select from "react-select";
 import PdfCard from "./pdfCard";
 
-
 const customStyles = {
-  control: base => ({
-    ...base,
-    height: 50,
-    borderRadius: 16,
-    fontFamily: 'Open Sans',
-    fontSize: 16,
-    color: '#303030',
-  })
+    control: base => ({
+        ...base,
+        height: 50,
+        borderRadius: 16,
+        fontFamily: "Open Sans",
+        fontSize: 16,
+        color: "#303030"
+    })
 };
 
-const useStyles = makeStyles((theme) => ({
-  cardTitle: {
-    fontSize: "18px",
-    paddingLeft: "5px",
-    marginBottom: "5px",
-    color: theme.palette.primary.dark,
-    fontFamily: "Open Sans",
-    fontWeight: "600",
-    ['@media (max-width:960px)']: {
-      fontSize: "14px"
+const useStyles = makeStyles(theme => ({
+    cardTitle: {
+        fontSize: "18px",
+        paddingLeft: "5px",
+        marginBottom: "5px",
+        color: theme.palette.primary.dark,
+        fontFamily: "Open Sans",
+        fontWeight: "600",
+        ["@media (max-width:960px)"]: {
+            fontSize: "14px"
+        },
+        ["@media (min-width:1600px)"]: {
+            fontSize: "20px"
+        }
     },
-    ['@media (min-width:1600px)']: {
-      fontSize: "20px",
-    },
-  },
-  pdfCard: {
-    padding: "15px",
-    borderRadius: "16px",
-    marginTop: "20px",
-    boxShadow: "0px 10px 25px rgba(48, 48, 48, 0.2)",
-  }
+    pdfCard: {
+        padding: "15px",
+        borderRadius: "16px",
+        marginTop: "20px",
+        boxShadow: "0px 10px 25px rgba(48, 48, 48, 0.2)"
+    }
 }));
 
 export default function MiddleSection() {
+    const classes = useStyles();
 
-  const classes = useStyles();
-  
-  const handleChange = (e) => {
-    setIndex(e.value)
-  }
+    const handleChange = e => {
+        setIndex(e.value);
+    };
 
-  const options = [
-    { value: 0, label: "2020-21" },
-    { value: 1, label: "2021-22" },
-    { value: 2, label: "2022-23" },
-  ]
+    const options = [
+        { value: 0, label: "2020-21" },
+        { value: 1, label: "2021-22" },
+        { value: 2, label: "2022-23" },
+        { value: 3, label: "2023-24" }
+    ];
 
-  // links should be in order with last one being the latest one
-  const links = [
-    "/documents/Tnp-Team/Executive-members-20-21.pdf",
-    "/documents/Tnp-Team/Executive-Members.pdf",
-    "/documents/Tnp-Team/Executives_2022-2023.pdf",
-  ];
+    // links should be in order with last one being the latest one
+    const links = [
+        "/documents/Tnp-Team/Executive-members-20-21.pdf",
+        "/documents/Tnp-Team/Executive-Members.pdf",
+        "/documents/Tnp-Team/Executives_2022-2023.pdf",
+        "/documents/Tnp-Team/Executives_2023-2024.pdf"
+    ];
 
-  const [index, setIndex] = useState(links.length - 1);
+    const [index, setIndex] = useState(links.length - 1);
 
-  return (
-    <>
-      <h3 className={classes.cardTitle} >Executive Members</h3>
-        <Select
-          styles={customStyles}
-          options={options}
-          onChange={handleChange}
-          label="batch"
-          defaultValue={options[options.length - 1]}
-        />
-      <Card className={classes.pdfCard}>
-        <PdfCard source={links[index]} />
-      </Card>
-    </>
-  );
+    return (
+        <>
+            <h3 className={classes.cardTitle}>Executive Members</h3>
+            <Select
+                styles={customStyles}
+                options={options}
+                onChange={handleChange}
+                label="batch"
+                defaultValue={options[options.length - 1]}
+            />
+            <Card className={classes.pdfCard}>
+                <PdfCard source={links[index]} />
+            </Card>
+        </>
+    );
 }
