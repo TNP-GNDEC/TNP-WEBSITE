e84b4d96faf234855afd8e9e3c51dbf4a5e1fd7e
DaveyDark <devesh97531@gmail.com>
2025-06-08 16:30:36 +0530

    Added new technical team information

--- resources/js/components/Pages/TechnicalMembers/TechnicalMembers.js
+++ resources/js/components/Pages/TechnicalMembers/TechnicalMembers.js
@@ -7,6 +7,7 @@ import Navbar from "../../HomeComponent/SideComponents/Navbar";
 import RightBar from "./components/RightBar";
 import LeftBar from "./components/LeftBar";
 import Scroll from "../../HomeComponent/SideComponents/scroll";
+import MemberCards26 from "./components/MemberCards26";
 import MemberCards25 from "./components/MemberCards25";
 import MemberCards24 from "./components/MemberCards24";
 import MemberCards23 from "./components/MemberCards23";
@@ -18,160 +19,152 @@ import MemberCards19 from "./components/MemberCards19";
 import Select from "react-select";
 
 const options = [
-    { value: "25", label: "2024-25" },
-    { value: "24", label: "2023-24" },
-    { value: "23", label: "2022-23" },
-    { value: "22", label: "2021-22" },
-    { value: "21", label: "2020-21" },
-    { value: "20", label: "2019-20" },
-    { value: "19", label: "2018-19" }
+  { value: "26", label: "2025-26" },
+  { value: "25", label: "2024-25" },
+  { value: "24", label: "2023-24" },
+  { value: "23", label: "2022-23" },
+  { value: "22", label: "2021-22" },
+  { value: "21", label: "2020-21" },
+  { value: "20", label: "2019-20" },
+  { value: "19", label: "2018-19" }
 ];
 
 const customStyles = {
-    control: base => ({
-        ...base,
-        height: 50,
-        borderRadius: 16,
-        fontFamily: "Open Sans",
-        fontSize: 16,
-        color: "#303030"
-    })
+  control: base => ({
+    ...base,
+    height: 50,
+    borderRadius: 16,
+    fontFamily: "Open Sans",
+    fontSize: 16,
+    color: "#303030"
+  })
 };
 
 const useStyles = makeStyles(theme => ({
-    body: {
-        backgroundColor: theme.palette.primary.light,
-        margin: "0px",
-        minHeight: "100vh"
+  body: {
+    backgroundColor: theme.palette.primary.light,
+    margin: "0px",
+    minHeight: "100vh"
+  },
+  root: {
+    flexGrow: 1,
+    margin: "15px 15px 0",
+    marginTop: "30px",
+    paddingTop: "20px"
+  },
+  paper: {
+    padding: theme.spacing(1),
+    backgroundColor: "transparent",
+    boxShadow: "none"
+  },
+  paper2: {
+    padding: theme.spacing(1),
+    backgroundColor: "transparent",
+    boxShadow: "none",
+    position: "sticky",
+    top: "80px"
+  },
+  left: {
+    marginTop: "40px",
+    width: "100%",
+    ["@media (max-width:960px)"]: {
+      display: "none"
     },
-    root: {
-        flexGrow: 1,
-        margin: "15px 15px 0",
-        marginTop: "30px",
-        paddingTop: "20px"
-    },
-    paper: {
-        padding: theme.spacing(1),
-        backgroundColor: "transparent",
-        boxShadow: "none"
-    },
-    paper2: {
-        padding: theme.spacing(1),
-        backgroundColor: "transparent",
-        boxShadow: "none",
-        position: "sticky",
-        top: "80px"
-    },
-    left: {
-        marginTop: "40px",
-        width: "100%",
-        ["@media (max-width:960px)"]: {
-            display: "none"
-        },
-        ["@media (min-width:1600px)"]: {
-            marginTop: "60px"
-        }
-    },
-    center: {
-        marginTop: "40px",
-        ["@media (max-width:960px)"]: {
-            width: "100%"
-        },
-        ["@media (min-width:1600px)"]: {
-            marginTop: "60px"
-        }
-    },
-    right: {
-        marginTop: "40px",
-        width: "100%",
-        ["@media (max-width:960px)"]: {
-            display: "none"
-        },
-        ["@media (min-width:1600px)"]: {
-            marginTop: "60px"
-        }
-    },
-    cardTitle: {
-        fontSize: "18px",
-        paddingLeft: "5px",
-        marginBottom: "5px",
-        ["@media (min-width:1600px)"]: {
-            fontSize: "20px"
-        },
-        color: theme.palette.primary.dark,
-        fontFamily: "Open Sans",
-        fontWeight: "600",
-        ["@media (max-width:960px)"]: {
-            fontSize: "14px"
-        }
-    },
-    MemCards: {
-        marginTop: "10px"
+    ["@media (min-width:1600px)"]: {
+      marginTop: "60px"
     }
+  },
+  center: {
+    marginTop: "40px",
+    ["@media (max-width:960px)"]: {
+      width: "100%"
+    },
+    ["@media (min-width:1600px)"]: {
+      marginTop: "60px"
+    }
+  },
+  right: {
+    marginTop: "40px",
+    width: "100%",
+    ["@media (max-width:960px)"]: {
+      display: "none"
+    },
+    ["@media (min-width:1600px)"]: {
+      marginTop: "60px"
+    }
+  },
+  cardTitle: {
+    fontSize: "18px",
+    paddingLeft: "5px",
+    marginBottom: "5px",
+    ["@media (min-width:1600px)"]: {
+      fontSize: "20px"
+    },
+    color: theme.palette.primary.dark,
+    fontFamily: "Open Sans",
+    fontWeight: "600",
+    ["@media (max-width:960px)"]: {
+      fontSize: "14px"
+    }
+  },
+  MemCards: {
+    marginTop: "10px"
+  }
 }));
 
 export default function TechnicalMembers() {
-    const classes = useStyles();
-    const [selectValue, setSelectValue] = React.useState();
+  const classes = useStyles();
+  const [selectValue, setSelectValue] = React.useState();
 
-    return (
-        <div className={classes.body}>
-            <Navbar />
-            <div className={classes.root}>
-                <Scroll showBelow={250} />
-                <Grid container spacing={3}>
-                    <Grid
-                        item
-                        md={3}
-                        className={classes.left}
-                        display={{ xs: "none", md: "block" }}
-                    >
-                        <Paper className={classes.paper}>
-                            <LeftBar />
-                        </Paper>
-                    </Grid>
-                    <Grid item md={6} className={classes.center}>
-                        <Paper className={classes.paper}>
-                            <h3 className={classes.cardTitle}>
-                                Technical Members
-                            </h3>
-                            <Select
-                                styles={customStyles}
-                                options={options}
-                                defaultValue={options[0]}
-                                onChange={val => setSelectValue(val.value)}
-                            />
-                            <Grid
-                                container
-                                spacing={2}
-                                className={classes.MemCards}
-                            >
-                                {!selectValue || selectValue === "25" ? (
-                                    <MemberCards25 />
-                                ) : (
-                                    ""
-                                )}
-                                {selectValue === "24" ? <MemberCards24 /> : ""}
-                                {selectValue === "23" ? <MemberCards23 /> : ""}
-                                {selectValue === "22" ? <MemberCards22 /> : ""}
-                                {selectValue === "21" ? <MemberCards21 /> : ""}
-                                {selectValue === "20" ? <MemberCards20 /> : ""}
-                                {selectValue === "19" ? <MemberCards19 /> : ""}
-                            </Grid>
-                        </Paper>
-                    </Grid>
-                    <Grid
-                        item
-                        md={3}
-                        className={classes.right}
-                        display={{ xs: "none", md: "block" }}
-                    >
-                        <Paper className={classes.paper}>
-                            <RightBar />
-                        </Paper>
-                    </Grid>
-                </Grid>
-            </div>
-        </div>
-    );
+  return (
+    <div className={classes.body}>
+      <Navbar />
+      <div className={classes.root}>
+        <Scroll showBelow={250} />
+        <Grid container spacing={3}>
+          <Grid
+            item
+            md={3}
+            className={classes.left}
+            display={{ xs: "none", md: "block" }}
+          >
+            <Paper className={classes.paper}>
+              <LeftBar />
+            </Paper>
+          </Grid>
+          <Grid item md={6} className={classes.center}>
+            <Paper className={classes.paper}>
+              <h3 className={classes.cardTitle}>Technical Members</h3>
+              <Select
+                styles={customStyles}
+                options={options}
+                defaultValue={options[0]}
+                onChange={val => setSelectValue(val.value)}
+              />
+              <Grid container spacing={2} className={classes.MemCards}>
+                {!selectValue || selectValue === "26" ? <MemberCards26 /> : ""}
+                {selectValue === "25" ? <MemberCards25 /> : ""}
+                {selectValue === "24" ? <MemberCards24 /> : ""}
+                {selectValue === "23" ? <MemberCards23 /> : ""}
+                {selectValue === "22" ? <MemberCards22 /> : ""}
+                {selectValue === "21" ? <MemberCards21 /> : ""}
+                {selectValue === "20" ? <MemberCards20 /> : ""}
+                {selectValue === "19" ? <MemberCards19 /> : ""}
+              </Grid>
+            </Paper>
+          </Grid>
+          <Grid
+            item
+            md={3}
+            className={classes.right}
+            display={{ xs: "none", md: "block" }}
+          >
+            <Paper className={classes.paper}>
+              <RightBar />
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 }
--- resources/js/components/Pages/TechnicalMembers/components/MemberCard.js
+++ resources/js/components/Pages/TechnicalMembers/components/MemberCard.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import Linkedin from '@material-ui/icons/LinkedIn';
-import Insta from '@material-ui/icons/Instagram';
-import Twitter from '@material-ui/icons/Twitter';
-import Facebook from '@material-ui/icons/Facebook';
-import Mail from '@material-ui/icons/Mail';
-import GitHub from '@material-ui/icons/GitHub';
-import Website from '@material-ui/icons/Language';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import Linkedin from "@material-ui/icons/LinkedIn";
+import Insta from "@material-ui/icons/Instagram";
+import Twitter from "@material-ui/icons/Twitter";
+import Facebook from "@material-ui/icons/Facebook";
+import Mail from "@material-ui/icons/Mail";
+import GitHub from "@material-ui/icons/GitHub";
+import Website from "@material-ui/icons/Language";
 
-
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     width: "100%",
     paddingTop: "15px",
@@ -18,9 +17,9 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     borderRadius: "16px",
     boxShadow: "0px 15px 30px #8a959e33",
-    ['@media (min-width:1600px)']: {
+    ["@media (min-width:1600px)"]: {
       paddingTop: "20px"
-    },
+    }
   },
   media: {
     textAlign: "center"
@@ -38,7 +37,7 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center"
   },
   Icon: {
-    fontSize: "18px",
+    fontSize: "18px"
   },
   imageBox: {
     marginLeft: "auto",
@@ -50,20 +49,21 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    ['@media (min-width:1600px)']: {
+    ["@media (min-width:1600px)"]: {
       height: "140px",
       width: "140px"
-    },
+    }
   },
   image: {
     width: "106px",
     height: "106px",
     borderRadius: "50%",
     boxShadow: "0px 10px 20px #1687d933",
-    ['@media (min-width:1600px)']: {
+    objectFit: "cover",
+    ["@media (min-width:1600px)"]: {
       height: "120px",
-      width: "120px",
-    },
+      width: "120px"
+    }
   },
   heading: {
     fontSize: "18px",
@@ -71,18 +71,18 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: "600",
     color: theme.palette.primary.dark,
     paddingTop: "12px",
-    ['@media (min-width:1600px)']: {
-      fontSize: "20px",
-    },
+    ["@media (min-width:1600px)"]: {
+      fontSize: "20px"
+    }
   },
   subheading: {
     fontSize: "13px",
     fontFamily: "Open Sans",
     fontWeight: "600",
     color: theme.palette.primary.text,
-    ['@media (min-width:1600px)']: {
-      fontSize: "16px",
-    },
+    ["@media (min-width:1600px)"]: {
+      fontSize: "16px"
+    }
   },
   box: {
     marginLeft: "auto",
@@ -96,9 +96,9 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     alignItems: "center",
     justifyContent: "flex-start",
-    ['@media (min-width:1600px)']: {
-      height: "36px",
-    },
+    ["@media (min-width:1600px)"]: {
+      height: "36px"
+    }
   },
   box2: {
     marginLeft: "auto",
@@ -111,18 +111,18 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     alignItems: "center",
     justifyContent: "flex-start",
-    ['@media (min-width:1600px)']: {
+    ["@media (min-width:1600px)"]: {
       height: "36px",
       marginBottom: "20px"
-    },
+    }
   },
   icon2: {
     fontSize: "16px",
     color: theme.palette.primary.dark,
     marginLeft: "10px",
-    ['@media (min-width:1600px)']: {
-      fontSize: "18px",
-    },
+    ["@media (min-width:1600px)"]: {
+      fontSize: "18px"
+    }
   },
   subheading2: {
     fontSize: "14px",
@@ -130,28 +130,40 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: "Open Sans",
     fontWeight: "600",
     color: theme.palette.primary.dark,
-    ['@media (min-width:1600px)']: {
-      fontSize: "16px",
-    },
+    ["@media (min-width:1600px)"]: {
+      fontSize: "16px"
+    }
   },
   description: {
     margin: "20px",
     fontFamily: "Open Sans",
     color: theme.palette.primary.text,
     height: "80px",
-    ['@media (max-width:1024px)']: {
-      height: "140px",
+    ["@media (max-width:1024px)"]: {
+      height: "140px"
     },
-    ['@media (max-width:768px)']: {
-      height: "80px",
+    ["@media (max-width:768px)"]: {
+      height: "80px"
     },
-    ['@media (max-width:376px)']: {
-      height: "90px",
-    },
-  },
+    ["@media (max-width:376px)"]: {
+      height: "90px"
+    }
+  }
 }));
 
-export default function MemberCard({ name, designation, description, profilePic, linkedIn, website, instagram, github, facebook, mail, twitter }) {
+export default function MemberCard({
+  name,
+  designation,
+  description,
+  profilePic,
+  linkedIn,
+  website,
+  instagram,
+  github,
+  facebook,
+  mail,
+  twitter
+}) {
   const classes = useStyles();
 
   return (
@@ -161,96 +173,76 @@ export default function MemberCard({ name, designation, description, profilePic,
           <img src={profilePic} className={classes.image} />
         </div>
       </div>
-      <h1 className={classes.heading}>
-        {name}
-      </h1>
-      <h3 className={classes.subheading}>
-        {designation}
-      </h3>
+      <h1 className={classes.heading}>{name}</h1>
+      <h3 className={classes.subheading}>{designation}</h3>
       <div className={classes.description}>
-        <p className={classes.descrip}>
-          {description}
-        </p>
+        <p className={classes.descrip}>{description}</p>
       </div>
       <div>
         <ul className="list-inline" id="list-link">
           {facebook ? (
             <li className="list-inline-item">
-              <a
-                className="social-icon"
-                target="_blank"
-                href={facebook}
-              >
+              <a className="social-icon" target="_blank" href={facebook}>
                 <Facebook />
               </a>
             </li>
-          ) : ""}
+          ) : (
+            ""
+          )}
           {twitter ? (
             <li className="list-inline-item mx-1">
-              <a
-                className="social-icon"
-                target="_blank"
-                href={twitter}
-              >
+              <a className="social-icon" target="_blank" href={twitter}>
                 <Twitter />
               </a>
             </li>
-          ) : ""}
+          ) : (
+            ""
+          )}
           {linkedIn ? (
             <li className="list-inline-item mx-1">
-              <a
-                className="social-icon"
-                target="_blank"
-                href={linkedIn}
-              >
+              <a className="social-icon" target="_blank" href={linkedIn}>
                 <Linkedin />
               </a>
             </li>
-          ) : ""}
+          ) : (
+            ""
+          )}
           {website ? (
             <li className="list-inline-item mx-1">
-              <a
-                className="social-icon"
-                target="_blank"
-                href={website}
-              >
+              <a className="social-icon" target="_blank" href={website}>
                 <Website />
               </a>
             </li>
-          ) : ""}
+          ) : (
+            ""
+          )}
           {instagram ? (
             <li className="list-inline-item mx-1">
-              <a
-                className="social-icon"
-                target="_blank"
-                href={instagram}
-              >
+              <a className="social-icon" target="_blank" href={instagram}>
                 <Insta />
               </a>
             </li>
-          ) : ""}
+          ) : (
+            ""
+          )}
           {mail ? (
             <li className="list-inline-item mx-1">
-              <a
-                className="social-icon"
-                target="_blank"
-                href={mail}
-              >
+              <a className="social-icon" target="_blank" href={mail}>
                 <Mail />
               </a>
             </li>
-          ) : ""}
+          ) : (
+            ""
+          )}
           {github ? (
             <li className="list-inline-item mx-1">
-              <a
-                className="social-icon"
-                target="_blank"
-                href={github}
-              >
+              <a className="social-icon" target="_blank" href={github}>
                 <GitHub />
               </a>
             </li>
-          ) : ""}
+          ) : (
+            ""
+          )}
         </ul>
       </div>
     </Card>
